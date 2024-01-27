bd76574683322d4584f962537fe03632bceda9e9
Jucaski <thanididbefore@gmail.com>
2024-01-27 00:12:13 -0600

    Adding design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChineseNovelsComponent } from './views/chinese-novels/chinese-novels.component';
+import { BookCoverComponent } from './components/book-cover/book-cover.component';
+
 
 const routes: Routes = [
+  {path: 'book', component: BookCoverComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChineseNovelsComponent } from './views/chinese-novels/chinese-novels.component';
+import { BookCoverComponent } from './components/book-cover/book-cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChineseNovelsComponent
+    ChineseNovelsComponent,
+    BookCoverComponent
   ],
   imports: [
     BrowserModule,
